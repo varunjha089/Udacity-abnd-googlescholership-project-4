2a85ac75ee782af590244863b6c85fb3ffd00119
bharatmk257 <bharatmk257@gmail.com>
2018-07-10 23:38:21 +0530

    ok so code is now working with list view adapter

--- app/src/main/java/com/example/bharatmk257/music/HomeFragment.java
+++ app/src/main/java/com/example/bharatmk257/music/HomeFragment.java
@@ -5,19 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class HomeFragment extends Fragment {
+
+    private SongListAdapter songAdapter;
+    private ListView listView;
+    private ArrayList<SongList> songLists;
 
 
     @Nullable
@@ -27,37 +29,38 @@ public class HomeFragment extends Fragment {
 
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViews();
+        loadData();
+    }
+
+    private void initViews(){
+        listView = getActivity().findViewById(R.id.listOfSong);
+    }
+
+    private void loadData(){
+
+        songLists = new ArrayList<>();
+        songLists.add(new SongList("one",R.drawable.playlist0));
+        songLists.add(new SongList("two",R.drawable.playlist0));
+        songLists.add(new SongList("three",R.drawable.playlist0));
+        songLists.add(new SongList("four",R.drawable.playlist0));
+        songLists.add(new SongList("five",R.drawable.playlist0));
+        songLists.add(new SongList("six",R.drawable.playlist0));
+        songLists.add(new SongList("seven",R.drawable.playlist0));
+        songLists.add(new SongList("eight",R.drawable.playlist0));
+        songLists.add(new SongList("nine",R.drawable.playlist0));
+        songLists.add(new SongList("ten",R.drawable.playlist0));
+
+        songAdapter = new SongListAdapter  (getActivity(), songLists);
+
+
+        listView.setAdapter(songAdapter);
+
+
+    }
+
+
 }
-
-//    public class HomeFragment extends AppCompatActivity {
-//
-//        @Override
-//        protected void onCreate(Bundle savedInstanceState) {
-//            super.onCreate(savedInstanceState);
-//            setContentView(R.layout.fragment_home);
-//
-//            ArrayList<SongList> songLists = new ArrayList<SongList>();
-//            songLists.add(new SongList("one",R.drawable.playlist0));
-//            songLists.add(new SongList("two",R.drawable.playlist0));
-//            songLists.add(new SongList("three",R.drawable.playlist0));
-//            songLists.add(new SongList("four",R.drawable.playlist0));
-//            songLists.add(new SongList("five",R.drawable.playlist0));
-//            songLists.add(new SongList("six",R.drawable.playlist0));
-//            songLists.add(new SongList("seven",R.drawable.playlist0));
-//            songLists.add(new SongList("eight",R.drawable.playlist0));
-//            songLists.add(new SongList("nine",R.drawable.playlist0));
-//            songLists.add(new SongList("ten",R.drawable.playlist0));
-//
-//
-//
-//            SongListAdapter songAdapter = new SongListAdapter(this, songLists);
-//
-//            ListView listView = (ListView) findViewById(R.id.listOfSong);
-//
-//            listView.setAdapter(songAdapter);
-//
-//        }
-//    }
-
-
-
